c763b418ef362aba467d3511ba11b835fbce5fbe
matomugo60 <matomugo60@gmail.com>
2023-02-16 13:57:34 +0300

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, color) VALUES ("Mr.Chocolate", "M", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Rowdy", "M", "Black");
+INSERT INTO bears (name, sex, color) VALUES ("Tabitha", "F", "Black");
+INSERT INTO bears (name, sex, color) VALUES ("Sergeant Brown", "F", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Melissa", "F", "Kodiak");
+INSERT INTO bears (name, sex, color) VALUES ("Grinch", "M", "Black");
+INSERT INTO bears (name, sex, color) VALUES ("Wendy", "F", "Brown");
+INSERT INTO bears (sex, color) VALUES ("F", "Black");
